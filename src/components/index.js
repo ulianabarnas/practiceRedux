b09fe27312414e326a59a98cdebe4af40e2dd114
Uliana Vasylchenko <ulianabarnas9225@gmail.com>
2022-11-27 12:42:49 +0200

    refactoring

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,14 @@
 export * from 'components/App/App';
 export * from 'components/Header/Header';
 export * from 'components/Header/Header.styled';
-export * from 'components/Heading/Heading.styled';
-export * from 'components/Section/Section.styled';
-export * from 'components/Container/Container.styled';
 export * from 'components/SearchForm/SearchForm';
 export * from 'components/SearchForm/SearchForm.styled';
-export * from 'components/Text/Text.styled';
-export * from 'components/Grid/Grid.styled';
 export * from 'components/Todo/Todo';
 export * from 'components/TodoList/TodoList';
+
+//UI components
+export * from 'components/UI/Grid/Grid.styled';
+export * from 'components/UI/Container/Container.styled';
+export * from 'components/UI/Heading/Heading.styled';
+export * from 'components/UI/Section/Section.styled';
+export * from 'components/UI/Text/Text.styled';
