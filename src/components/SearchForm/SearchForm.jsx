ff1ad14b88548732782bdc4e6d78f2f9027ad8be
Uliana Vasylchenko <ulianabarnas9225@gmail.com>
2022-11-27 12:38:32 +0200

    add deleteTodo

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,47 +1,89 @@
-import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+import { useDispatch } from 'react-redux';
+import { addTodoAction } from 'redux/todoSlice';
+
+import { useLocalStorage } from 'hooks';
 
 import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
-export class SearchForm extends Component {
-  state = {
-    query: '',
-  };
+export const SearchForm = () => {
+  const dispatch = useDispatch();
+  
+  const [value, setValue] = useLocalStorage('search', '');
 
-  handleInput = e => {
-    this.setState({
-      query: e.currentTarget.value,
-    });
-  };
+  const handleInput = ({ target }) => setValue(target.value);
 
-  handleSubmit = e => {
-    const { query } = this.state;
-
+  const handleSubmit = e => {
     e.preventDefault();
 
-    this.props.onSubmit(query);
-
-    this.setState({
-      query: '',
-    });
-  };
-  render() {
-    const { query } = this.state;
-
-    return (
-      <SearchFormStyled onSubmit={this.handleSubmit}>
-        <FormBtn type="submit">
-          <FiSearch size="16px" />
-        </FormBtn>
-        <InputSearch
-          onChange={this.handleInput}
-          placeholder="What do you want to write?"
-          name="search"
-          required
-          value={query}
-          autoFocus
-        />
-      </SearchFormStyled>
+    dispatch(
+      addTodoAction({
+        id: nanoid(),
+        text: value,
+      })
     );
-  }
-}
+
+    setValue('');
+  };
+
+  return (
+    <SearchFormStyled onSubmit={handleSubmit}>
+      <FormBtn type="submit">
+        <FiSearch size="16px" />
+      </FormBtn>
+      <InputSearch
+        onChange={handleInput}
+        placeholder="What do you want to write?"
+        name="search"
+        required
+        value={value}
+        autoFocus
+      />
+    </SearchFormStyled>
+  );
+};
+
+// export class SearchForm extends Component {
+//   state = {
+//     query: '',
+//   };
+
+//   handleInput = e => {
+//     this.setState({
+//       query: e.currentTarget.value,
+//     });
+//   };
+
+//   handleSubmit = e => {
+//     const { query } = this.state;
+
+//     e.preventDefault();
+
+//     this.props.onSubmit(query);
+
+//     this.setState({
+//       query: '',
+//     });
+//   };
+//   render() {
+//     const { query } = this.state;
+
+//     return (
+//       <SearchFormStyled onSubmit={this.handleSubmit}>
+//         <FormBtn type="submit">
+//           <FiSearch size="16px" />
+//         </FormBtn>
+//         <InputSearch
+//           onChange={this.handleInput}
+//           placeholder="What do you want to write?"
+//           name="search"
+//           required
+//           value={query}
+//           autoFocus
+//         />
+//       </SearchFormStyled>
+//     );
+//   }
+// }
--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,19 +1,24 @@
+import { useDispatch } from 'react-redux';
+import { deleteTodoAction } from 'redux/todoSlice';
+
 import { RiDeleteBinLine } from 'react-icons/ri';
 import { Text } from 'components';
 import { DeleteButton, TodoWrapper } from './Todo.styled';
 
-export const Todo = ({ text, counter, onClick, id }) => {
+export const Todo = ({ text, counter, id }) => {
+  const dispatch = useDispatch();
+
+  const handlerDeleteTodo = () => dispatch(deleteTodoAction(id));
+
   return (
-    <>
-      <TodoWrapper>
-        <Text textAlign="center" marginBottom="20px">
-          TODO #{counter}
-        </Text>
-        <Text>{text}</Text>
-        <DeleteButton type="button" onClick={() => onClick(id)}>
-          <RiDeleteBinLine size={24} />
-        </DeleteButton>
-      </TodoWrapper>
-    </>
+    <TodoWrapper>
+      <Text textAlign="center" marginBottom="20px">
+        TODO #{counter}
+      </Text>
+      <Text>{text}</Text>
+      <DeleteButton type="button" onClick={handlerDeleteTodo}>
+        <RiDeleteBinLine size={24} />
+      </DeleteButton>
+    </TodoWrapper>
   );
 };
